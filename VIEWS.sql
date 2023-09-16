6bc4dcceb575c6e95eb85ffc0df47bd7d97fb28f
Raphael Nicaise <raphanicaise@gmail.com>
2023-09-16 09:45:57 -0300

    Update VIEWS.sql

--- VIEWS.sql
+++ VIEWS.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW total_price_for_each_product AS (
-	SELECT pr.product_name, pr.price,inv.quantity, (price*quantity) AS totalPrice 
+	SELECT concat(pr.product_name,' ',pr.description), pr.price,inv.quantity, 
+    (price*quantity) AS totalPrice 
 	FROM PRODUCTS pr LEFT JOIN INVENTORY inv ON  pr.id_product = inv.id_product
 );
 
